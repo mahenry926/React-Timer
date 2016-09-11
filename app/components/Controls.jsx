1c6a7bf3937a46709fd4135712d287b32834aa8a
mahenry <user@Matthew-Henrys-rMBP.local>
2016-09-10 20:55:20 -0700

    Update status to stop when count reaches zero

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 
 var Controls = React.createClass({
-
+  componentWillReceiveProps: function (newProps) {
+    console.log("Component will receive props", newProps.countdownStatus);
+  },
   propTypes: {
     countdownStatus: React.PropTypes.string.isRequired,
     onStatusChange: React.PropTypes.func.isRequired
